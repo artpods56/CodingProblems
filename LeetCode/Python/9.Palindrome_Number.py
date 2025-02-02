82d3e9596da526088300a7b4ef6181146465757f
Artur Podsiadły <artpods56@gmail.com>
2025-02-02 22:54:05 +0100

    solution: complete

--- LeetCode/Python/9.Palindrome_Number.py
+++ LeetCode/Python/9.Palindrome_Number.py
@@ -1,10 +1,12 @@
 #lc_9:02_02_2025
+#https://leetcode.com/problems/palindrome-number/description/
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0 or (x > 0 and x%10 == 0):
             return False
         rev = 0
+        tmp = x
         if x < 0:
             return False
         while x > rev:
@@ -12,5 +14,4 @@ class Solution:
             rev = rev * 10 + mod
             x = x // 10
         
-        return x == rev or x == rev // 10
-        
+        return x == rev or x == rev // 10       
