d1ff361ad2ba33fcde1867ff6735fadc594354a8
Artur Podsiadły <artpods56@gmail.com>
2025-02-05 10:38:59 +0100

    solution: could be further improved

--- LeetCode/Python/1790.Check_If_One_String_Swap_Can_Make_Strings_Equal.py
+++ LeetCode/Python/1790.Check_If_One_String_Swap_Can_Make_Strings_Equal.py
@@ -4,33 +4,15 @@
 
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        s1 = list(s1)
-        s2 = list(s2)
-        set_s1 = set(list(s1))
-        set_s2 = set(list(s2))
-
-        differences = [s_1 == s_2 for s_1, s_2 in zip(s1, s2)]
-        diff_count = differences.count(False)
-        pointer = 0
-        indexes = []
-        for i in range(diff_count):
-            pointer = differences.index(False, pointer)
-            indexes.append(pointer)
-            pointer += 1
-
-        if s1 == s2:
+        diffs = [
+            i for i, (char_1, char_2) in enumerate(zip(s1, s2)) if char_1 != char_2
+        ]
+        if len(diffs) == 0:
             return True
-        if len(indexes) != 2:
-            return False
-
-        x1, x2 = indexes
-        s1[x1], s1[x2] = s1[x2], s1[x1]
-
-        if s1 == s2:
-            return True
-        print(s1, s2)
-        print(indexes)
-        if set_s1.difference(set_s2) == set() and diff_count <= 2:
+        elif len(diffs) == 2:
+            x1, x2 = diffs
+            s1, s2 = list(s1), list(s2)
+            s1[x1], s1[x2] = s1[x2], s1[x1]
             if s1 == s2:
                 return True
             else:
