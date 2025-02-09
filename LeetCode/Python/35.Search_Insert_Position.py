9c62a8c95690acab4f0b4926e7a9e595d0a41f55
Artur Podsiadły <artpods56@gmail.com>
2025-02-09 23:02:28 +0100

    solution: complete / remove unused variable

--- LeetCode/Python/35.Search_Insert_Position.py
+++ LeetCode/Python/35.Search_Insert_Position.py
@@ -6,7 +6,6 @@ class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         if target < nums[0]:
             return 0
-        i = 0
         for i in range(len(nums)):
             if nums[i] >= target:
                 return i
